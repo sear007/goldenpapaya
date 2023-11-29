099982e1d4140b67cd63794d36bb9eb0437d7c6c
Koung Buntha <sear@Koungs-MacBook-Pro.local>
2023-11-29 22:11:47 +0700

    update

--- page-about.php
+++ page-about.php
@@ -7,7 +7,6 @@ get_template_part('app/sections/page', 'header', array(
     'name' => 'About Us',
     'link' => 'about',
 ));
-get_template_part('app/sections/home', 'booking');
 get_template_part('app/sections/home', 'about');
 get_template_part('app/sections/home', 'team');
 get_footer();
--- page-contact.php
+++ page-contact.php
@@ -7,6 +7,5 @@ get_template_part('app/sections/page', 'header', array(
     'name' => 'Contact Us',
     'link' => 'contact',
 ));
-get_template_part('app/sections/home', 'booking');
 get_template_part('app/sections/page', 'contact');
 get_footer();
