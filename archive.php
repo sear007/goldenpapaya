8084930c8280ea3399950d3b5b7581e04e28b676
Buntha <buntha135.infinity@gmail.com>
2024-01-07 12:04:31 +0700

    update

--- archive.php
+++ archive.php
@@ -2,7 +2,10 @@
 get_header(); // Include the header template
 
 if (have_posts()) {
-    get_template_part('app/sections/home', 'room', array('title' => ''));
+    while (have_posts()) {
+        the_post();
+        get_template_part('app/sections/home', 'room', array('title' => ''));
+    }
 } else {
     echo '<p class="p-2 mt-5 mb-5">No posts found.</p>';
 }
